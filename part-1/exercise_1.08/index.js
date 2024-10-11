d1a1350d29857e26cac8e0fa952e37cef35e8e35
Juho Siitonen <104775829+JuhoSiitonen@users.noreply.github.com>
2024-10-11 21:55:57 +0000

    added basics of exercise 1.08

---
+++ part-1/exercise_1.08/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello DevOps with Kubernetes!</h1>')
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
