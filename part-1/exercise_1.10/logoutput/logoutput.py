6f7c06ff09c2ef22902370baf453367319258861
Juho <juho_tuomas@msn.com>
2024-10-20 22:49:42 +0300

    first setup for exercise 1.10

---
+++ part-1/exercise_1.10/logoutput/logoutput.py
@@ -0,0 +1,21 @@
+import time
+import random
+import string
+from datetime import datetime
+
+def generate_random_string(length=8):
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for _ in range(length))
+
+random_string = generate_random_string()
+
+def print_string_with_timestamp():
+    while True:
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"{timestamp} - {random_string}")
+        with open('/shared/output.txt', 'w') as file:
+            file.write(f"{timestamp} - Hash: {hashed}\n")
+        time.sleep(5)
+
+if __name__ == "__main__":
+    print_string_with_timestamp()
---
+++ part-1/exercise_1.10/logsender/logsender.py
@@ -0,0 +1,21 @@
+import time
+import random
+import string
+from datetime import datetime
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_string_with_timestamp():
+    try:
+        with open('/shared/output.txt', 'r') as file:
+            content = file.read().strip()
+            print(f"Read from file: {content}")
+    except FileNotFoundError:
+        print("File not found. Waiting for writer...")
+        
+    return jsonify({'response': content})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3001, debug=False)
