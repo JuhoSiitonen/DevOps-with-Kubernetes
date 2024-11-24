c31d4155a9f1dbe14d7981e0e662819a86fe3015
Juho <juho_tuomas@msn.com>
2024-11-24 16:52:24 +0200

    updated exercise 4.06

--- part-4/exercise_4.06/broadcaster/index.js
+++ part-4/exercise_4.06/broadcaster/index.js
@@ -1,6 +1,6 @@
 const NATS = require('nats')
 const natsCodec = NATS.StringCodec()
-const botKey = process.env.BROADCASTER_KEY
+const webHook = process.env.WEBHOOK
 
 const natsURL = process.env.NATS_URL || 'nats://nats:4222'
 
@@ -8,7 +8,16 @@ NATS.connect({servers: natsURL})
     .then(async (conn) => {
         const sub = conn.subscribe('todo-data', { queue: 'broadcaster.workers' }) 
         for await (const message of sub) {
-            console.log(`Sending message: ${natsCodec.decode(message.data)}`)
-            await fetch(`https://api.telegram.org/bot${botKey}/sendMessage?chat_id=1761737461&text=${natsSC.decode(message.data)}`)
+            let messageDecoded = natsCodec.decode(message.data)
+            console.log(`Sending message: ${messageDecoded}`)
+            try {
+                await fetch(webHook, {
+                  method: 'POST',
+                  headers: { 'Content-Type': 'application/json' },
+                  body: JSON.stringify({ content: messageDecoded })
+                });
+              } catch (error) {
+                console.error('Error in broadcaster:', error);
+              }
         }
     })
